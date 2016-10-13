fd97ee9f85691a167d243efc5051badb7c782b41
Neal Sanche <thorinside@gmail.com>
2016-10-12 21:40:20 -0600

    Upgraded Realm to 2.0.2

--- mobile/src/main/java/org/nsdev/apps/transittamer/modules/UserModule.java
+++ mobile/src/main/java/org/nsdev/apps/transittamer/modules/UserModule.java
@@ -46,8 +46,8 @@ public class UserModule {
 
     @UserScope
     @Provides
-    RealmConfiguration getRealmConfiguration(App context) {
-        RealmConfiguration config = new RealmConfiguration.Builder(context)
+    RealmConfiguration getRealmConfiguration() {
+        RealmConfiguration config = new RealmConfiguration.Builder()
                 .deleteRealmIfMigrationNeeded()
                 .build();
 
